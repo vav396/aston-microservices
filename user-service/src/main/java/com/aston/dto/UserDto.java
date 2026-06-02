41a50dbac9837dac1eb2024db501078d98de30a6
Веденёв Александр <vav396vav@mail.ru>
2026-06-02 19:21:00 +0300

    добавлен Swagger и HATEOAS в User Service

--- user-service/src/main/java/com/aston/dto/UserDto.java
+++ user-service/src/main/java/com/aston/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.aston.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +8,19 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "DTO для представления пользователя")
 public class UserDto {
 
+    @Schema(description = "Идентификатор пользователя", example = "1")
     private Long id;
+
+    @Schema(description = "Имя пользователя", example = "Иван Иванов", required = true)
     private String name;
+
+    @Schema(description = "Email пользователя", example = "ivan@example.com", required = true)
     private String email;
+
+    @Schema(description = "Возраст пользователя", example = "25", minimum = "0")
     private int age;
 
 }
